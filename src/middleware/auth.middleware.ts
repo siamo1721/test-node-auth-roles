b438b24e37f9c8bb3de3bd455f18e7dccc46ee1e
siamo1721 <siamocsgo@gmail.com>
2025-09-05 16:23:57 +0300

    feat(auth): добавление middleware для проверки ролей пользователя

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,9 @@
+exports.checkRole = (role) => {
+    return (req, res, next) => {
+        if (req.user && req.user.role === role) {
+            next();
+        } else {
+            res.status(403).json({ message: 'Нет доступа' });
+        }
+    };
+}
